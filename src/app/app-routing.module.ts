8db81cf2e45ecacab4835ab0818c37bba66e276c
Hao Hsieh <Hao.Hsieh@vertiv.com>
2022-04-08 15:34:50 +0800

    feat: add todo routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'todos',
+    pathMatch: 'full',
+  },
+  {
+    path: 'todos',
+    loadChildren: () => import('./features/todos/pages/todolist/todolist.module').then((mod) => mod.TodolistModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
